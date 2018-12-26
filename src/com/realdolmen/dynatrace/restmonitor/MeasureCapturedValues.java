0e99968299619b2d045c86e6e9f6329e10eda09a
Gabriel B Casella <gbc921@gmail.com>
2018-12-26 00:29:14 -0200

    Plugin handle json definite paths now
    But it only get first value of array
    "Ain't Nobody Got Time for That!"

--- src/com/realdolmen/dynatrace/restmonitor/MeasureCapturedValues.java
+++ src/com/realdolmen/dynatrace/restmonitor/MeasureCapturedValues.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.function.BiConsumer;
 import java.util.logging.Logger;
+import java.util.List;
 
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathExpression;
@@ -32,10 +33,10 @@ public class MeasureCapturedValues {
 	private static final String METRIC_NAME = "capturedValue";
 	private static final String PARAM_XJSONPATH = "xjsonPath";
 	private static final String PARAM_CONVERSIONMAP = "conversionMap";
-	
+
 	private static final Logger log = Logger.getLogger(RestMonitor.class.getName());
 	private static final String DEFAULT_RESULT = "0.0";
-	
+
 	private MonitorEnvironment monitorEnvironment;
 
 	/**
@@ -45,17 +46,23 @@ public class MeasureCapturedValues {
 		log.info("constructor");
 		this.monitorEnvironment = env;
 	}
-	
+
 	void applyMeasuresToEnvironment(String body, ResponseFormat format) throws RestMonitorConfigurationException{
 		Collection<MonitorMeasure> measures;
-		
+
 		log.info("applyMeasuresToEnvironment");
-		
+
 		if ((measures = monitorEnvironment.getMonitorMeasures(METRIC_GROUP, METRIC_NAME)) != null) {
 			log.info("measures count " + measures.size());
 			for (MonitorMeasure measure : measures){
 				String pathExpression = measure.getParameter(PARAM_XJSONPATH);
 				String conversionMapJson = measure.getParameter(PARAM_CONVERSIONMAP);
+
+				log.fine("Path Expression: " + pathExpression);
+				log.fine("Conversion Map: " + conversionMapJson);
+				log.fine("Response Format: " + format.toString());
+				log.fine("Response Body: " + body.toString());
+
 				Map<String, Double> conversionMap = null;
 				if (!StringUtil.isTrimmedEmpty(conversionMapJson)){
 					conversionMap = createConversionMap(conversionMapJson);
@@ -71,10 +78,10 @@ public class MeasureCapturedValues {
 			}
 		}
 	}
-	
+
 	/**
-	 * Converts the provided JSON string to a Map which can be used for String to Double conversions 
-	 * 
+	 * Converts the provided JSON string to a Map which can be used for String to Double conversions
+	 *
 	 * @param json configuration
 	 * @return the parsed JSON as Map
 	 * @throws RestMonitorConfigurationException
@@ -87,11 +94,11 @@ public class MeasureCapturedValues {
 		} catch (Exception e) {
 			throw new RestMonitorConfigurationException("ConversionMap parse error: " + e.getMessage());
 		}
-		
+
 		Map<String, Double> map = new HashMap<String, Double>();
 		try {
 			obj.forEach(new BiConsumer<String, Object>() {
-	
+
 				@Override
 				public void accept(String key, Object mapped) {
 					Double value = null;
@@ -105,34 +112,52 @@ public class MeasureCapturedValues {
 						value = Double.valueOf((String)mapped);
 					}
 					map.put(key, value);
-					
+
 				}
 			});
 		}catch (NumberFormatException e){
 			throw new RestMonitorConfigurationException("ConversionMap parse error: " + e.getMessage());
 		}
-		
+
 		return map;
 	}
 
 	/**
-	 * 
+	 *
 	 * @param jsonBody
 	 * @param jsonPathExpression
 	 * @param conversionMap
 	 * @return
 	 */
 	Double extractJSONPathValue(String jsonBody, String jsonPathExpression, Map<String, Double> conversionMap) throws RestMonitorConfigurationException{
+		Object obj = null;
 		JsonPath jsonPath = JsonPath.compile(jsonPathExpression);
-		if (!jsonPath.isDefinite()){
-			throw new RestMonitorConfigurationException("jsonPath should be definite");
-		}
+		
 		if (jsonBody.isEmpty()){
 			throw new RestMonitorConfigurationException("json response was empty!");
 		}
-		
-		Object obj = jsonPath.read(jsonBody);
-		
+		if (!jsonPath.isDefinite()){
+			log.fine("jsonPathIsDefinite");
+			List<Object> objList = jsonPath.read(jsonBody);
+			log.finer("object list" + objList.toString());
+			//workaround to get only one value to not deal with this now
+			try {
+				obj = objList.get(0);
+				log.finer("object list 1st position" + objList.get(0).toString());
+			} catch (Exception e) {
+				throw new RestMonitorConfigurationException("JSON XPath parse error: " + e.getMessage());
+			}
+		}
+
+		// check if it was not set yet, and try to set it
+		if (obj == null){
+			try {
+				obj = jsonPath.read(jsonBody);
+			} catch (Exception e) {
+				throw new RestMonitorConfigurationException("JSON read error: " + e.getMessage());
+			}
+		}
+
 		if (conversionMap==null){
 			if (obj instanceof Double){
 				return (Double)obj;
@@ -150,21 +175,21 @@ public class MeasureCapturedValues {
 				return Double.valueOf((String)obj);
 			}
 		}
-		
+
 		String key = obj.toString();
 		return conversionMap.get(key);
 	}
-	
+
 	/**
-	 * Select the xPathExpression from the xmlBody. 
+	 * Select the xPathExpression from the xmlBody.
 	 * If the conversionMap is null, the result is a double if it can be parsed to a double.
 	 * If the conversionMap is provided, the result must be a string that is mapped to a double via the conversionMap
-	 * 
+	 *
 	 * @param xmlBody
 	 * @param xPathExpression
 	 * @param conversionMap
 	 * @return
-	 * @throws RestMonitorConfigurationException 
+	 * @throws RestMonitorConfigurationException
 	 */
 	Double extractXPathValue(String xmlBody, String xPathExpression, Map<String, Double> conversionMap) throws RestMonitorConfigurationException{
 		InputSource inputSource = new InputSource( new StringReader(xmlBody));
@@ -194,5 +219,5 @@ public class MeasureCapturedValues {
 		}
 		return d;
 	}
-		
+
 }
